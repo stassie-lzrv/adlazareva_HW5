1861707539103e3c66aaccb474d31a33bee10ee8
stassie-lzrv <adlazareva@edu.hse.ru>
2022-12-07 22:29:32 +0300

    Initial Commit

---
+++ adlazareva_HW5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  adlazareva_HW5
+//
+//  Created by Настя Лазарева on 07.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
